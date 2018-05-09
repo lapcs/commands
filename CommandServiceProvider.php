715373fe7e23270ecfdc8f04d7d8e3d7ddc792dc
lavacs <mrtan1511@gmail.com>
2018-05-09 13:42:58 +0700

    create service provider

---
+++ CommandServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Lapcs\Commands;
+
+use Illuminate\Support\ServiceProvider;
+
+class CommandServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
